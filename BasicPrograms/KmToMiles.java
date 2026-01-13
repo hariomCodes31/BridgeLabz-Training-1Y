d8dd7f1307081a4e03dd4628484765159ac5f011
hariomCodes31 <hariomsingh9696822@gmail.com>
2026-01-13 14:10:11 +0530

    Update Km to Miles program

--- BasicPrograms/KmToMiles.java
+++ BasicPrograms/KmToMiles.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class kmtomiles{
+class KmToMiles{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the distance in km");
