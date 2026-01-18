f9c68f681039127a2537315406042d8f53bbe679
hariomCodes31 <hariomsingh9696822@gmail.com>
2026-01-18 11:38:59 +0530

    Added Age program to Level 1 (Primitive Data Types)

---
+++ BasicPrograms/Primitive-Data-Types/Level 1/Age.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Age{
+public static void main(String[]args){
+Scanner sc= new Scanner(System.in);
+System.out.println("Enter the current year");
+int a= sc.nextInt();
+int b= a-2000;
+System.out.println("harry's age is "+b);
+}
+}
