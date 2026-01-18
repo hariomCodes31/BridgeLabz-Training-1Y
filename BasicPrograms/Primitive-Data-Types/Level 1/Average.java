63bdb47b3e4baadfffd92151cbdd8086063ef779
hariomCodes31 <hariomsingh9696822@gmail.com>
2026-01-18 11:53:05 +0530

    Added Average program to Level 1

---
+++ BasicPrograms/Primitive-Data-Types/Level 1/Average.java
@@ -0,0 +1,11 @@
+public class Average {
+    public static void main(String[] args) {
+        int m = 94;
+        int p= 95;
+        int c = 96;
+
+        double average = (maths + physics + chemistry) / 3.0;
+
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
