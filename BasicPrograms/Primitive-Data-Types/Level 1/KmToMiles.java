8ca3657d3cb658e2c6ec14c49ac810393129456b
hariomCodes31 <hariomsingh9696822@gmail.com>
2026-01-18 12:00:06 +0530

    Added KmToMiles program to Level 1

---
+++ BasicPrograms/Primitive-Data-Types/Level 1/KmToMiles.java
@@ -0,0 +1,8 @@
+public class KmToMiles {
+    public static void main(String[] args) {
+        double km = 10.8;
+        double m = km * 1.6;
+
+        System.out.println("The distance " + km + " km in miles is " +m);
+    }
+}
