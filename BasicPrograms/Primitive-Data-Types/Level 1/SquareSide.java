3e4186f97de74c3e9736e3def0742f9ed3c01485
hariomCodes31 <hariomsingh9696822@gmail.com>
2026-01-18 14:13:52 +0530

    Added perimeter program to level 1

---
+++ BasicPrograms/Primitive-Data-Types/Level 1/SquareSide.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SquareSide {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter perimeter");
+        int p = sc.nextInt();
+
+        int s = p / 4;
+
+        System.out.println("The length of the side is " + s + " whose perimeter is " + p);
+    }
+}
