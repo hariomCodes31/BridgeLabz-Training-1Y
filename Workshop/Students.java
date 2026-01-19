461e2af7598c74c565874f541ecedc3f950b5452
hariomCodes31 <hariomsingh9696822@gmail.com>
2026-01-19 14:56:11 +0530

    Add Students program in Workshop

---
+++ Workshop/Students.java
@@ -0,0 +1,20 @@
+class Students{
+  int id;
+  String name;
+  int age;
+  void display(){
+  String status="Active";
+  System.out.println("id is "+id);
+  System.out.println("name is "+name);
+  System.out.println("age is "+age);
+  System.out.println("Status "+status);
+  }
+  public static void main(String[]args){
+    Students s=new Students();
+	s.id=100;
+	s.name="hariom";
+	s.age=20;
+	s.display();
+	}
+	}
+	
