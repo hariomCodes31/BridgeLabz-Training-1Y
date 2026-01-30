02441b5249573ef2f7611be6c5482514f99c4677
hariomCodes31 <hariomsingh9696822@gmail.com>
2026-01-30 15:00:47 +0530

    Added continue program to workshop

---
+++ Workshop/SkipEven.java
@@ -0,0 +1,14 @@
+class SkipEven{
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+
+            if (i%2==0) {
+                continue;
+            }
+
+            System.out.println(i);
+            }
+			}
+			}
+
