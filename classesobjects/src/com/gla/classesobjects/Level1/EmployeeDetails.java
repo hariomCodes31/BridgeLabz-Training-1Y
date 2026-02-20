4e64ba23635301559b8c31797a5ba3f7b87542e3
hariomCodes31 <hariomsingh9696822@gmail.com>
2026-02-20 22:37:24 +0530

    added level 1 to classes object

---
+++ classesobjects/src/com/gla/classesobjects/Level1/EmployeeDetails.java
@@ -0,0 +1,25 @@
+package com.gla.classesobjects.Level1;
+class Employee {
+    private String name;
+    private int id;
+    private double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Salary: " + salary);
+    }
+}
+
+public class EmployeeDetails {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Honey", 101, 50000);
+        emp.displayDetails();
+    }
+}
---
+++ classesobjects/src/com/gla/classesobjects/Level1/InventoryTracker.java
@@ -0,0 +1,31 @@
+package com.gla.classesobjects.Level1;
+class Item {
+    private int itemCode;
+    private String itemName;
+    private double price;
+
+    public Item(int itemCode, String itemName, double price) {
+        this.itemCode = itemCode;
+        this.itemName = itemName;
+        this.price = price;
+    }
+
+    public double calculateTotalCost(int quantity) {
+        return price * quantity;
+    }
+
+    public void displayDetails(int quantity) {
+        System.out.println("Item Code: " + itemCode);
+        System.out.println("Item Name: " + itemName);
+        System.out.println("Price: " + price);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Total Cost: " + calculateTotalCost(quantity));
+    }
+}
+
+public class InventoryTracker {
+    public static void main(String[] args) {
+        Item item = new Item(101, "Laptop", 50000);
+        item.displayDetails(2);
+    }
+}
